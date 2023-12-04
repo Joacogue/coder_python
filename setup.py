4ffefd071a65ca1fda8173b1e23d4c404b635883
Joaquin Guerrieri <joague10000@gmail.com>
2023-12-04 19:03:59 -0300

    agregando setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name= "primer_paquete",
+    version= "1.0",
+    description= "clase, metodos y paquete distribuido",
+    author= "Joaquin Guerrieri",
+    author_email= "joague10000@gmail.com",
+    packages= ["CLIENTES"],
+)
